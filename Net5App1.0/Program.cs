91753b07fb002f7d7dc91bba5bb734c551c69c7e
Timothy Axford <t.axford2@yahoo.com>
2022-12-04 12:58:24 -0500

    Added comments to explain what writeline and Read are for as well as what ASCII stands for

--- Net5App1.0/Program.cs
+++ Net5App1.0/Program.cs
@@ -18,7 +18,7 @@ namespace Net5App1._0
             int num2 = 23;
             int sum = num1 + num2;
 
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");//Prints and displays on console
             Console.WriteLine("Hello Tim!");
             
             Console.WriteLine(age);
@@ -26,7 +26,7 @@ namespace Net5App1._0
             Console.WriteLine("num1 " + num1 + " + num2 " + num2 + " is " + sum);
 
 
-            Console.Read();
+            Console.Read();//Takes a single input of string and returns the ASCII(American Standard Code for Information Interchange) value
         }
     }
 }
