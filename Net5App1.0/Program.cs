ff58e0ab0940748771b818ef01b226a3eecb8621
Timothy Axford <t.axford2@yahoo.com>
2022-12-04 01:05:15 -0500

    Added int variable into Main

--- Net5App1.0/Program.cs
+++ Net5App1.0/Program.cs
@@ -4,12 +4,16 @@ namespace Net5App1._0
 {
     class Program
     {
+        
         // Entry point of our program
         static void Main(string[] args)
         {
+            int age = 15;
+
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello Tim!");
             Console.Read();
+            Console.WriteLine(age);
         }
     }
 }
